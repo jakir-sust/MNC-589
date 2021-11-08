0251b8f0f2bcaeabaa382b9be9bba9d5599212e3
Mohammad Jakir Hossain <mh267@stones.cse.buffalo.edu>
2021-11-08 12:49:09 -0500

    Remove ipv6 part

--- cse489589_assignment1/mh267/src/ip_address.cpp
+++ cse489589_assignment1/mh267/src/ip_address.cpp
@@ -73,34 +73,14 @@ string  get_ip() {
         if (!ifa->ifa_addr) {
             continue;
         }
-        if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
-            // is a valid IP4 Address
+        if (ifa->ifa_addr->sa_family == AF_INET) {
             tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
             char addressBuffer[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-            //printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);
 
             string arr = string(ifa->ifa_name);
-
-            //std::cout <<"Type ==  "<< typeid(arr).name() << '\n';
 
             if ("enp2s4" == arr)
-            {
-                return std::string(addressBuffer);
-            }
-
-            //if (strcmp("enp2s4",ifa->ifa_name) == 0) {
-              //  return std::string(addressBuffer);
-           // }
-
-        } else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
-            // is a valid IP6 Address
-            tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
-            char addressBuffer[INET6_ADDRSTRLEN];
-            inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
-            //printf("%s IP Address %s\n", ifa->ifa_name, addressBuffer);
-            string arr = string(ifa->ifa_name);
-             if ("enp2s4" == arr)
             {
                 return std::string(addressBuffer);
             }
